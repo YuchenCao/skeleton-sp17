bb62ec90946ffd21bee257514ff247fd6defe15a
Yuchen Cao <3130103343@zju.edu.cn>
2017-02-17 16:31:49 +0800

    Completed LeapYear

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -14,6 +14,13 @@ public class LeapYear {
         }
     }
 
+    public static boolean isLeapYear(int year) {
+        if (year % 4 != 0) {
+            return false;
+        }
+        return true;
+    }
+
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
